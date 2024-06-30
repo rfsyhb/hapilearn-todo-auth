67ba76702fdfc98560fc1dbad80351258fc1f918
rfsyhb <rafisyihab3@gmail.com>
2024-06-30 17:50:49 +0700

    token needed for todo routes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -14,20 +14,32 @@ module.exports = [
     method: 'GET',
     path: '/todos/{id}',
     handler: todoController.getTodoById,
+    options: {
+      pre: [{ method: verifyToken }],
+    },
   },
   {
     method: 'POST',
     path: '/todos',
     handler: todoController.createTodo,
+    options: {
+      pre: [{ method: verifyToken }],
+    },
   },
   {
     method: 'PUT',
     path: '/todos/{id}',
     handler: todoController.updateTodo,
+    options: {
+      pre: [{ method: verifyToken }],
+    },
   },
   {
     method: 'DELETE',
     path: '/todos/{id}',
     handler: todoController.deleteTodo,
+    options: {
+      pre: [{ method: verifyToken }],
+    },
   },
 ];
